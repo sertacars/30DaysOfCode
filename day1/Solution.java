894c5921c292b642d909e89d4e667ecabf60a14c
Sertac Aras <arass@cardiff.ac.uk>
2021-01-04 21:48:09 +0300

    Read and save values to my variables

--- day1/Solution.java
+++ day1/Solution.java
@@ -8,17 +8,19 @@ public class Solution {
         double d = 4.0;
         String s = "HackerRank ";
 
-        Scanner scan = new Scanner(System.in);
-
         /* Declare second integer, double, and String variables. */
-        int number;
-        double doubleNumber;
-        String name;
-
-
         /* Read and save an integer, double, and String to your variables. */
         // Note: If you have trouble reading the entire String, please go back and
         // review the Tutorial closely.
+
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        double doubleNumber = scan.nextDouble();
+        String name = scan.next();
+
+        System.out.println(number);
+        System.out.println(doubleNumber);
+        System.out.println(name);
 
         /* Print the sum of both integer variables on a new line. */
 
