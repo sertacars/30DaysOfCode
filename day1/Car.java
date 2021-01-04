44c24f498b3ee502516b9daecc024c79fb36036e
Sertac Aras <arass@cardiff.ac.uk>
2021-01-04 21:09:08 +0300

    Creating car properties

---
+++ day1/Car.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class Car {
+
+    int maxSpeed;
+    int minSpeed;
+    double weight;
+    boolean isTheCarOn;
+
+    public void public static void main(String[] args) {
+        
+
+
+    }
+    
+}
