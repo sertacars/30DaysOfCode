d44adc51cb4061989f22d5895be455b0a626a641
Sertac Aras <arass@cardiff.ac.uk>
2021-01-05 17:54:56 +0300

    Finalized solution with math round method

--- day2/Solution.java
+++ day2/Solution.java
@@ -6,6 +6,11 @@ public class Solution {
     // Complete the solve function below.
     static void solve(double meal_cost, int tip_percent, int tax_percent) {
 
+        meal_cost = meal_cost + (meal_cost * tip_percent)/100 + (meal_cost * tax_percent)/100 ;
+
+        System.out.println(Math.round(meal_cost));
+
+
     }
 
     private static final Scanner scanner = new Scanner(System.in);
