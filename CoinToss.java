acb8e26d2d73e519d6e6772e66a95ddc3eb627da
Sertac Aras <arass@cardiff.ac.uk>
2021-01-06 17:44:40 +0300

    Writea method to get a random number & results as 0 or 1 based on its divisible of 2

--- CoinToss.java
+++ CoinToss.java
@@ -4,6 +4,8 @@ public class CoinToss {
 
     public int tossACoin() {
         Random rand = new Random();
+        int toss = Math.abs(rand.nextInt()) % 2;
+        return toss;
     }
 
     public static void public static void main(String[] args) {
