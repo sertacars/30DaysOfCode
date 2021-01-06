2ca9cc19081c000ffe6488e923e0d54b4fe1bc3f
Sertac Aras <arass@cardiff.ac.uk>
2021-01-06 18:04:37 +0300

    Finalize solution using if & else if statements

--- day3/Solution.java
+++ day3/Solution.java
@@ -8,7 +8,21 @@ public class Solution {
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
         scanner.close();
+
+        if (N % 2 == 1){
+            System.out.println("Weird");
+        }
+        else if(2 <= N && N <= 5){
+            System.out.println("Not Weird");
+        }
+        else if (6 <= N && N <= 20) {
+            System.out.println("Weird");
+        }
+        else if (N > 20) {
+            System.out.println("Not Weird");
+        }
+
+
     }
 }
