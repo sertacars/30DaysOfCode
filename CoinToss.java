4774e181d1335297c562895ba1d3cbd5af883fd5
Sertac Aras <arass@cardiff.ac.uk>
2021-01-06 17:41:07 +0300

    Create a random java method to toss a coin

--- CoinToss.java
+++ CoinToss.java
@@ -1,4 +1,11 @@
+import java.util.Random;
+
 public class CoinToss {
+
+    public int tossACoin() {
+        Random rand = new Random();
+    }
+
     public static void public static void main(String[] args) {
         
     }
