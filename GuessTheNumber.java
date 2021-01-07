f05cd3a932304dbf209e9cb11aa79de6ab618f17
Sertac Aras <arass@cardiff.ac.uk>
2021-01-07 19:52:08 +0300

    Write a play method to get users guess and print out how close it is to the random number

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -15,6 +15,16 @@ public class GuessTheNumber {
     }
 
     public void play() {
+        int move = scanner.nextInt();
+        if (move > theNumber){
+            System.out.println("Your number is too big");
+        }
+        else if (move < theNumber){
+            System.out.println("Your number is too small");
+        }
+        else{
+            System.out.println("You got it!!!!!!");
+        }
 
     }
 
