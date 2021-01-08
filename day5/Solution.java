c6e5e24782d91ebbe89f73f669bfee05c36ade33
Sertac Aras <arass@cardiff.ac.uk>
2021-01-08 18:08:33 +0300

    Import day 5 problem

---
+++ day5/Solution.java
@@ -0,0 +1,14 @@
+package day5;
+
+import java.util.Scanner;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
