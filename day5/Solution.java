c3e01869548604f73e409f3e7b6f9d265a643b97
Sertac Aras <arass@cardiff.ac.uk>
2021-01-08 18:19:52 +0300

    Finalize the solution

--- day5/Solution.java
+++ day5/Solution.java
@@ -9,6 +9,11 @@ public class Solution {
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+
+        for (int i=1; i<=10; i++){
+            System.out.println(n + " x " + i + " = " + n*i);
+        }
+
         scanner.close();
     }
 }
