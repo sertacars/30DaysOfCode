dab51d1c1540327f136b15b2d369f53cad5605cd
Sertac Aras <arass@cardiff.ac.uk>
2021-01-08 17:48:31 +0300

    Create project to practise loops

---
+++ day4/LoopPractise.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class LoopPractise {
+
+    public void practiseWhileLoop(){
+        int x = 0;
+        while(x < 5){
+            System.out.println("The value of x is :" + x);
+            x++;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
