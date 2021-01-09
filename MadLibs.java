17fba72591e8753c5177cb6bca81fd82bfc5b765
Sertac Aras <arass@cardiff.ac.uk>
2021-01-09 16:54:03 +0300

    Write getter and setters for all properties

--- MadLibs.java
+++ MadLibs.java
@@ -16,6 +16,78 @@ public class MadLibs {
     String randomNums;
     Random rand = new Random();
 
+    public String getStory(){
+        return story;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getAdjective1() {
+        return adjective1;
+    }
+    
+    public String getAdjective2() {
+        return adjective2;
+    }
+    
+    public String getNoun1() {
+        return noun1;
+    }
+    
+    public String getNoun2() {
+        return noun2;
+    }
+    
+    public String getNoun3() {
+        return noun3;
+    }
+    
+    public String getAdverb() {
+        return adverb;
+    }
+    
+    public String getRandomNums() {
+        return randomNums;
+    }
+
+    public void setStory(String newStory){
+        this.story = newStory;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+    
+    public void setAdjective1(String newAdjective1) {
+        this.adjective1 = newAdjective1;
+    }
+
+    public void setAdjective2(String newAdjective2) {
+        this.adjective2 = newAdjective2;
+    }
+
+    public void setNoun1(String newNoun1) {
+        this.noun1 = newNoun1;
+    }
+    
+    public void setNoun2(String newNoun2) {
+        this.noun2 = newNoun2;
+    }
+    
+    public void setNoun3(String newNoun3) {
+        this.noun3 = newNoun3;
+    }
+
+    public void setAdverb(String newAdverb) {
+        this.adverb = newAdverb;
+    }
+    
+    public void setRandNums() {
+
+    }
+
     public static void main(String[] args) {
         
     }
