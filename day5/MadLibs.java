edba5eddf31d426b063c894139340e8fcbf79d67
Sertac Aras <arass@cardiff.ac.uk>
2021-01-09 16:43:02 +0300

    Start creating a mad libs program from scratch

---
+++ day5/MadLibs.java
@@ -0,0 +1,12 @@
+package day5;
+
+import java.util.Scanner;
+
+public class MadLibs {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+}
