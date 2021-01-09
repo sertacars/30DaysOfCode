99c19058478863251ca94279e61b4556a451ab90
Sertac Aras <arass@cardiff.ac.uk>
2021-01-09 16:46:09 +0300

    Create necessary variables

--- MadLibs.java
+++ MadLibs.java
@@ -1,9 +1,20 @@
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class MadLibs {
 
     Scanner scanner = new Scanner(System.in);
+    String story;
+    String name;
+    String adjective1;
+    String adjective2;
+    String noun1;
+    String noun2;
+    String noun3;
+    String adverb;
+    String randomNums;
+    Random rand = new Random();
 
     public static void main(String[] args) {
         
