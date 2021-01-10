48a6970919f723f456769492ee83eac254e40761
Sertac Aras <arass@cardiff.ac.uk>
2021-01-10 19:47:58 +0300

    Checking pointer differences

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -24,6 +24,7 @@ public class ArrayPractice {
         //declaring, allocating and initializing arrays
         int[] intArray1;
         int[] intArray2 = new int[4];
+        printArray(intArray2);
         int[] intArray3 = {5, 2, 9, 1, 3};
 
         String[] shoppingList = {"bananas", "apples", "pears"};
