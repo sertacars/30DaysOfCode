2bb981132ba3a8597ce658395b9388a7cd948934
Sertac Aras <arass@cardiff.ac.uk>
2021-01-10 19:39:04 +0300

    Printing out arrays

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ArrayPractice {
     public static void main(String[] args) {
 
@@ -5,10 +7,16 @@ public class ArrayPractice {
         //      [0, 5,  3,  2 , 1] --> length 5
 
 
+        //declaring, allocating and initializing arrays
         int[] intArray1;
         int[] intArray2 = new int[4];
         int[] intArray3 = {5, 2, 9, 1, 3};
 
+        String[] shoppingList = {"bananas", "apples", "pears"};
+
+        //print out arrays
+        System.out.println(Arrays.toString(intArray2));
+        System.out.println(Arrays.toString(intArray3));
 
     }
     
