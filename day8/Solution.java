5f491cfb3c06364c468d2358d052cf70c9955111
Sertac Aras <arass@cardiff.ac.uk>
2021-01-11 19:53:46 +0300

    Finalize the solution

--- day8/Solution.java
+++ day8/Solution.java
@@ -1,19 +1,28 @@
 package day8;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] argh) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        Map<String, Integer> phoneBook = new HashMap<String, Integer>();
         for (int i = 0; i < n; i++) {
             String name = in.next();
             int phone = in.nextInt();
             // Write code here
+            phoneBook.put(name, phone);
         }
         while (in.hasNext()) {
             String s = in.next();
             // Write code here
+            if (phoneBook.containsKey(s)){
+                System.out.println(s + "=" + phoneBook.get(s));
+            }else {
+                System.out.println("Not found");
+            }
         }
         in.close();
     }
