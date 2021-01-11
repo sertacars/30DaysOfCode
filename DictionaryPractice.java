86e629a489980c2705b92db59be88b7b2df9a12e
Sertac Aras <arass@cardiff.ac.uk>
2021-01-11 18:26:36 +0300

    Creating english to spanish dictionary using dictionaries and hashmaps

--- DictionaryPractice.java
+++ DictionaryPractice.java
@@ -1,5 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class DictionaryPractice {
     public static void main(String[] args) {
+        //english to spanish dictionary
+        Map<String,String> englSpanDict = new HashMap<String,String>();
+
+        //putting things inside dict
+        englSpanDict.put("Monday","Lunes");
+        englSpanDict.put("Tuesday","Martes");
+        englSpanDict.put("Wednesday", "Miercoles");
+        englSpanDict.put("Thursday", "Jueves");
+        englSpanDict.put("Friday", "Viernes");
+
+        //retrieve thigns from dict
+
         
+
     }
 }
