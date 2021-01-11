7230c0e36597cddb6b26d1691ce961e18c09efd2
Sertac Aras <arass@cardiff.ac.uk>
2021-01-11 18:38:07 +0300

    Complete the weeks and add extra lines for next dictionary

--- DictionaryPractice.java
+++ DictionaryPractice.java
@@ -12,6 +12,8 @@ public class DictionaryPractice {
         englSpanDict.put("Wednesday", "Miercoles");
         englSpanDict.put("Thursday", "Jueves");
         englSpanDict.put("Friday", "Viernes");
+        englSpanDict.put("Saturday", "Sabado");
+        englSpanDict.put("Sunday", "Domingo");
 
         //retrieve thigns from dict
         System.out.println(englSpanDict.get("Monday"));
@@ -25,6 +27,12 @@ public class DictionaryPractice {
 
         //print out all values
         System.out.println(englSpanDict.values());
+
+        //print out sizes
+        System.out.println("Size of dictionary is: " + englSpanDict.size());
+
+        System.out.println();
+        System.out.println();
 
     }
 }
