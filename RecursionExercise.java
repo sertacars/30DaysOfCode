b07e9353cd81679e78f802936dee768bf3d6257c
Sertac Aras <arass@cardiff.ac.uk>
2021-01-12 20:06:49 +0300

    Write code to calculate and print out factorial

--- RecursionExercise.java
+++ RecursionExercise.java
@@ -10,21 +10,31 @@ public class RecursionExercise {
     public static int Summation (int n) {
         // base case : we are at the end
         if (n <= 0){
-            return 0;
+            return 0; //additive identity property
         }
         // recursive case : keep going
         else {
-            return n +  Summation(n-1);
+            return n + Summation(n-1);
         }
 
     }
 
-
+    public static int Factorial(int n){
+        // base case
+        if (n <= 1){
+            return 1;
+        }
+        // recursive case
+        else {
+            return n * Factorial(n-1);
+        }
+    }
 
 
 
     public static void main(String[] args) {
 
         System.out.println(Summation(3));
+        System.out.println(Factorial(4));
     }
 }
