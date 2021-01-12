c938d87b44ce4c46d060c9e474e96fc64f12b045
Sertac Aras <arass@cardiff.ac.uk>
2021-01-12 19:59:11 +0300

    Write code to calculate summation

--- RecursionExercise.java
+++ RecursionExercise.java
@@ -5,6 +5,20 @@ public class RecursionExercise {
     // a=20 --> f(20) = 25
     // f(f(f(20))) = f(f(25)) = f(30) = 35
 
+    // summation of 5 = 5+4+3+2+1;
+
+    public static int Summation (int n) {
+        // base case : we are at the end
+        if (n <= 0){
+            return 0;
+        }
+        // recursive case : keep going
+        else {
+            return n +  Summation(n-1);
+        }
+
+    }
+
 
 
 
