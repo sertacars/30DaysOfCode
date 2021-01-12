cdcc83e0e25d9e46febba0a6f35780a7127745b3
Sertac Aras <arass@cardiff.ac.uk>
2021-01-12 19:53:50 +0300

    Practising recursion

---
+++ RecursionExercise.java
@@ -0,0 +1,15 @@
+public class RecursionExercise {
+
+    // f(f(f(a))) = ?
+    // f(a) = 5 + a
+    // a=20 --> f(20) = 25
+    // f(f(f(20))) = f(f(25)) = f(30) = 35
+
+
+
+
+
+    public static void main(String[] args) {
+        
+    }
+}
