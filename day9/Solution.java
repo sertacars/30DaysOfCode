65805e7b4cc4b341538a76dd220d231dec89204e
Sertac Aras <arass@cardiff.ac.uk>
2021-01-12 21:12:57 +0300

    Import day 9 problem template

---
+++ day9/Solution.java
@@ -0,0 +1,27 @@
+package day9;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    // Complete the factorial function below.
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = factorial(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
