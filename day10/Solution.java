2c39158ee32415d8cbc71f1482c21c3f83fce6ea
Sertac Aras <arass@cardiff.ac.uk>
2021-01-13 21:08:39 +0300

    Finalize the solution

--- day10/Solution.java
+++ day10/Solution.java
@@ -3,12 +3,33 @@ package day10;
 import java.util.Scanner;
 
 public class Solution {
-    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        scanner.close();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int remainder;
+        int numOfOnes=0;
+        int totalConsOnes=0;
+        while (n > 0) {
+            remainder = n % 2;
+            n = n / 2;
+            if (remainder == 1) {
+                numOfOnes++;
+                if (numOfOnes >= totalConsOnes)
+
+                    totalConsOnes = numOfOnes;
+
+            } else {
+
+                numOfOnes = 0;
+            }
+        }
+
+        System.out.println(totalConsOnes);
+                
+            in.close();
+        }
+
+        
     }
-}
