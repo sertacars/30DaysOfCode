84391a9c70ea21b2fc6844de8efaf738e73c9ca6
Sertac Aras <arass@cardiff.ac.uk>
2021-01-15 21:38:45 +0300

    Create library variables and constructor

--- LibraryCatalogue.java
+++ LibraryCatalogue.java
@@ -1,6 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class LibraryCatalogue {
 
+    // properties, fields, global variables
+    Map<String,Book> bookCollection = new HashMap<String,Book>();
+    int currentDay = 0;
+    int lengthOfCheckedOutPeriod = 7;
+    double initialLateFee = 0.50;
+    double feePerLateDay = 1.00;
+
+    public LibraryCatalogue(Map<String,Book> collection){
+        this.bookCollection = collection;
+    }
+
+    public LibraryCatalogue(Map<String,Book> collection, int lengthOfCheckedOutPeriod, double initialLateFee, double feePerLateDay){
+        this.bookCollection = collection;
+        this.lengthOfCheckedOutPeriod = lengthOfCheckedOutPeriod;
+        this.initialLateFee = initialLateFee;
+        this.feePerLateDay = feePerLateDay;
+    }
     public static void main(String[] args) {
         
     }
