8cf559eccaaa5adc2223c479da33dce57d6c3994
Sertac Aras <arass@cardiff.ac.uk>
2021-01-15 21:19:43 +0300

    Create properties that defines the Book

---
+++ Book.java
@@ -0,0 +1,13 @@
+public class Book {
+
+    String title;
+    int pageCount;
+    int ISBN;
+    boolean isCheckedOut;
+    int dayCheckedOut = -1;
+    
+    public Book(String bookTtitle, int bookPageCount, int bookISBN) {
+
+    }
+
+}
