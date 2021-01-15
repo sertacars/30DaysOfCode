3bfe26a5c94c1c93bea521c753a05ac26943235c
Sertac Aras <arass@cardiff.ac.uk>
2021-01-15 21:25:50 +0300

    Create constructor and getter methods

--- Book.java
+++ Book.java
@@ -1,13 +1,41 @@
 public class Book {
 
+    // properties, fields, global variables
     String title;
     int pageCount;
     int ISBN;
     boolean isCheckedOut;
     int dayCheckedOut = -1;
     
+    // constructor
     public Book(String bookTtitle, int bookPageCount, int bookISBN) {
+        this.title = bookTtitle;
+        this.pageCount = pageCount;
+        this.ISBN = ISBN;
+        isCheckedOut = false;
+    }
+
+    // getters
+    public String getTitle(){
+        return this.title;
 
     }
+    
+    public int getPageCount() {
+        return this.pageCount;
+    }
+    
+    public int getISBN() {
+        return this.ISBN;
+    }
+    
+    public boolean getIsCheckedOut() {
+        return this.isCheckedOut;
+    }
+    
+    public int getDayCheckedOut() {
+        return this.dayCheckedOut;
+    }
+
 
 }
