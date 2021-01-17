89d47cca4c624e549e0aae8d30b8135a128c3bd2
Sertac Aras <arass@cardiff.ac.uk>
2021-01-17 18:16:06 +0300

    Create a cat class which extends animal

---
+++ Cat.java
@@ -0,0 +1,17 @@
+public class Cat extends Animal {
+
+    public Cat (int age) {
+        super(7);
+        System.out.println("A cat has been created.");
+    }
+
+    public void meow(){
+
+        System.out.println("Cat says meows");
+    }
+
+    public void prance(){
+        System.out.println("A cat is prancing");
+    }
+    
+}
