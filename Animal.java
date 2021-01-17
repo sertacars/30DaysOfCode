a23fa7f18393b5526686fcb5a982c735c507c289
Sertac Aras <arass@cardiff.ac.uk>
2021-01-17 18:10:43 +0300

    Writing code to practise inheritance

---
+++ Animal.java
@@ -0,0 +1,15 @@
+
+public class Animal {
+
+    public Animal () {
+        System.out.println("Animal has been created.");
+    }
+
+    public void eat(){
+        System.out.println("An animal is eating.");
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,9 @@
+public class Dog extends Animal{
+
+    public Dog () {
+
+        super(); //referencing animal constructor in animal class
+        System.out.println("A Dog has been created.");
+    }
+    
+}
