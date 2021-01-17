c80fa56cbc9c00d4bdb543c053595d6b3b9ec778
Sertac Aras <arass@cardiff.ac.uk>
2021-01-17 22:21:31 +0300

    Finalize the solution

--- day12/Student.java
+++ day12/Student.java
@@ -1,5 +1,7 @@
 package day12;
 
+import java.util.Arrays;
+
 class Student extends Person {
     private int[] testScores;
 
@@ -16,10 +18,40 @@ class Student extends Person {
      */
     // Write your constructor here
 
+    public Student (String firstName, String lastName, int id, int[] testScores){
+
+        super(firstName, lastName, id);
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = id;
+        this.testScores = testScores;
+    }
+
     /*
      * Method Name: calculate
      * 
      * @return A character denoting the grade.
      */
     // Write your method here
+
+    public char calculate() {
+        double score = Arrays.stream(testScores).average().orElse(Double.NaN);
+        if (score >= 90 && score <= 100) {
+            return 'O';
+        } else if (score >= 80 && score < 90) {
+            return 'E';
+        } else if (score >= 70 && score < 80) {
+            return 'A';
+        } else if (score >= 55 && score < 70) {
+            return 'P';
+        } else if (score >= 40 && score < 55) {
+            return 'D';
+        } else if (score < 40) {
+            return 'T';
+        } else
+            return 'X';
+    }
+
+
 }
